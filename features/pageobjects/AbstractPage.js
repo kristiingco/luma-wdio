39bb2e8b3df3542f20192a3ef4d7f5832774bd97
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-03-24 20:42:48 -0700

    Fix typo in AbstractPage

--- features/pageobjects/AbstractPage.js
+++ features/pageobjects/AbstractPage.js
@@ -4,6 +4,6 @@ module.exports = class AbstractPage {
      * @param path path of the sub page (e.g. /path/to/page.html)
      */
     open(path = "") {
-        return browser.url(`https://the-internet.herokuapp.com/${path}`);
+        return browser.url(`/${path}`);
     }
 };
