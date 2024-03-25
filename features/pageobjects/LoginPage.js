b6d0e33478124f40ef0f323783138730376c6ff8
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-03-24 20:57:22 -0700

    Add getter methods to LoginPage

--- features/pageobjects/LoginPage.js
+++ features/pageobjects/LoginPage.js
@@ -2,7 +2,23 @@ const AbstractPage = require("./AbstractPage");
 
 class LoginPage extends AbstractPage {
     open() {
-        return super.open("login");
+        return super.open("customer/account/login/");
+    }
+
+    get emailInput() {
+        return $("#email");
+    }
+
+    get passwordInput() {
+        return $("[name='login[password]']");
+    }
+
+    get signInButton() {
+        return $(".action.login.primary");
+    }
+
+    get forgotYourPasswordLink() {
+        return $("a[class='action remind'] span");
     }
 }
 
